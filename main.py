e68e2c8e62e36ceaad5825802a65dab0236cf5b8
GameFox8 <38018799+GameFox8@users.noreply.github.com>
2020-02-09 04:30:09 +0300

    init

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ path = 'employees/'
 def main():
     files = [r + x for r, d, f in os.walk(path) for x in f]
 
-    db = dp.Postgre(database='postgres', user='postgres', password='456789boks')
-    db.create_database('test_1')    # создат базу данных с нужной струтурой для работы
+    db = dp.Postgre(database='test_1', user='postgres', password='456789boks')
+    db.clear_data()
+    #db.create_database('test_1')    # создат базу данных с нужной струтурой для работы
 
     for file in files:
         for project in parse_projects(file):
             db.add_project(project)
 
     variance_rating = db.get_variance_rating()
-    print('Рейтинг сотрудников по среднеквадратическому отклонению человекодней от плана:')
+    print('Рейтинг сотрудников по \nсреднеквадратическому отклонению человекодней от плана:')
     for id_e, fio, variance in variance_rating:
-        print('fio: {:18s} \t variance: {:10g}'.format(fio, variance))
+        print('ФИО: {:18s} \t Отклонение: {:10g}'.format(fio, variance))
 
 
 if __name__ == '__main__':
